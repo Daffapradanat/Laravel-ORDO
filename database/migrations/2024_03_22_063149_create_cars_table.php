b6387aede436f8a363743afb935a13a2caf4ea4d
Daffa Pradana Tsaqif <160875540+Daffapradanat@users.noreply.github.com>
2024-03-22 16:09:48 +0700

    Migrations

--- database/migrations/2024_03_22_063149_create_cars_table.php
+++ database/migrations/2024_03_22_063149_create_cars_table.php
@@ -4,24 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateCarsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('cars', function (Blueprint $table) {
             $table->id();
+            $table->string('nama');
+            $table->string('jenis');
+            $table->decimal('harga', 20, 2);
+            $table->date('tanggal_pembuatan');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('cars');
     }
-};
+}
