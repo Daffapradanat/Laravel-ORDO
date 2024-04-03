ef31444f3849765678f46d22632e66177a6e88dd
Daffa Pradana Tsaqif <160875540+Daffapradanat@users.noreply.github.com>
2024-04-03 17:42:36 +0700

    CarsSeeder

--- database/seeders/CarsSeeder.php
+++ database/seeders/CarsSeeder.php
@@ -3,20 +3,65 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
+use App\Models\Cars;
 
 class CarsSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     *
+     * @return void
      */
     public function run()
     {
-        DB::table('cars')->insert([
-            'nama' => 'Innova',
+        Cars::create([
+            'nama' => 'Xenia',
             'jenis' => 'MPV',
-            'harga' => 250000000,
-            'tanggal_pembuatan' => '2023-01-15',
+            'harga' => 200000000,
+            'tanggal_pembuatan' => '2022-01-01',
         ]);
+
+        Cars::create([
+            'nama' => 'Avanza',
+            'jenis' => 'MPV',
+            'harga' => 210000000,
+            'tanggal_pembuatan' => '2022-02-01',
+        ]);
+
+        Cars::create([
+            'nama' => 'Ford',
+            'jenis' => 'Sedan',
+            'harga' => 300000000,
+            'tanggal_pembuatan' => '2022-03-01',
+        ]);
+
+        Cars::create([
+            'nama' => 'Ferrari F8',
+            'jenis' => 'Supercar',
+            'harga' => 3000000000,
+            'tanggal_pembuatan' => '2022-04-01',
+        ]);
+
+        Cars::create([
+            'nama' => 'Ford Mustang',
+            'jenis' => 'Muscle Car',
+            'harga' => 400000000,
+            'tanggal_pembuatan' => '2022-05-01',
+        ]);
+
+        Cars::create([
+            'nama' => 'Lamborghini Huracan',
+            'jenis' => 'Supercar',
+            'harga' => 3500000000,
+            'tanggal_pembuatan' => '2022-06-01',
+        ]);
+
+        Cars::create([
+            'nama' => 'Tesla',
+            'jenis' => 'Electric Car',
+            'harga' => 500000000,
+            'tanggal_pembuatan' => '2022-07-01',
+        ]);
+
     }
 }
